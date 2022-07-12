be02e5734709d93f5853a0e183f01358632d9a0d
Kailey09 <kcoop1323@gmail.com>
2022-07-11 23:32:07 -0400

    added thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -33,3 +33,43 @@ const ReactionSchema = new Schema(
         id: false,
     }
 );
+
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: "Thought is required",
+            minlength: 1,
+            maxlength: 280,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        reactions: [ReactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
